323de8b127c9e3366405b5d0d5a109c8cc817767
EvanAristy <evanjaristy@gmail.com>
2022-03-01 16:52:36 -0500

    Did the model, controller, and repo for Users table

--- capstoneProject/src/main/java/com/messegeboard/backend/exceptions/ResourceNotFoundException.java
+++ capstoneProject/src/main/java/com/messegeboard/backend/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,6 @@
 package com.messegeboard.backend.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
