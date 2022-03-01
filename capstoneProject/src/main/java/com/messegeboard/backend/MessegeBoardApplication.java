bd7a130851d4430d5caeea0b7993eedeb6fea039
EvanAristy <evanjaristy@gmail.com>
2022-03-01 14:54:28 -0500

    first push

---
+++ capstoneProject/src/main/java/com/messegeboard/backend/MessegeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.messegeboard.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessegeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessegeBoardApplication.class, args);
+	}
+
+}
---
+++ capstoneProject/src/main/java/com/messegeboard/backend/models/User.java
@@ -0,0 +1,54 @@
+package com.messegeboard.backend.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+	@Id
+	@Column
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Column
+	private String firstname;
+	@Column
+	private String lastname;
+	@Column
+	private String nickname;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
+}
