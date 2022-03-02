b2f3604631e69d782d4f995fae8a8142f978012f
EvanAristy <evanjaristy@gmail.com>
2022-03-02 15:15:17 -0500

    Completed backend, for now

--- capstoneProject/src/main/java/com/messageboard/backend/models/Message.java
+++ capstoneProject/src/main/java/com/messageboard/backend/models/Message.java
@@ -1,29 +1,56 @@
 package com.messageboard.backend.models;
 
+import java.util.Date;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="messages")
 public class Message {
 
 	@Id
-	@Column
+	@Column(name="id")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
-	@Column
+	@Column(name="post")
 	private String post;
-	@Column
-	private String time;
-	@Column
-	private String date;
-	@Column
-	private int user_id;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="date")
+	private Date date;
+//	@Column(name="user_id")
+//	private int user_id;
 	
+	@PrePersist
+	private void onCreate() {
+		this.date=new Date();
+	}
+	
+//	Creating one to one relationship between user and message
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -36,17 +63,25 @@ public class Message {
 		this.post = post;
 	}
 	
-	public String getTime() {
-		return time;
+	public void setId(int id) {
+		this.id = id;
 	}
-	
-	public String getDate() {
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+//	public void setUser_id(int user_id) {
+//		this.user_id = user_id;
+//	}
+
+	public Date getDate() {
 		return date;
 	}
 	
-	public int getUser_id() {
-		return user_id;
-	}
+//	public int getUser_id() {
+//		return user_id;
+//	}
 		
 	
 }
